1aa6d821ad3dfff6fe02ce9e40829a719d981009
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2025-01-04 00:18:40 +0530

    bug fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -27,7 +27,7 @@ export default function RootLayout({ children }) {
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
         <head>
-          <ThemeModeScript/>         
+          <ThemeModeScript />
         </head>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
