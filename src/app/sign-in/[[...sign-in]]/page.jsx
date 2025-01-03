870aa4124b9e10816f3e8c9c34f8961bb0eb7156
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2025-01-03 23:20:55 +0530

    custom signIn clerk

--- src/app/sign-in/[[...sign-in]]/page.jsx
+++ src/app/sign-in/[[...sign-in]]/page.jsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="">
+    <div className="flex items-center justify-center p-3 m-4">
       <SignIn />
     </div>
   );
