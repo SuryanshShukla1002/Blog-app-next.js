9507b40c131a3fb3d73050013ba4993d33bdbd17
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2025-01-07 04:27:30 +0530

    user model created

---
+++ src/lib/models/user.model.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+    {
+        clerId: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        firstName: {
+            type: String,
+            required: true,
+        },
+        lastName: {
+            type: String,
+            required: true,
+        },
+        Username: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        profilePicture: {
+            type: String,
+            required: true,
+        },
+        isAdmin: {
+            type: Boolean,
+            default: true,
+        }
+    }, { timestamps: true }
+);
+
+const User = mongoose.models.User || mongoose.model('User', userSchema);
+export default User;
